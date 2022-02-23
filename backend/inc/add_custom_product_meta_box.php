37b72ce4187a0fda322f866bd946de81bb2598cd
Asadurzzaman <asadur.dxn2u@gmail.com>
2022-02-23 23:46:46 +0600

    product attribute

--- backend/inc/add_custom_product_meta_box.php
+++ backend/inc/add_custom_product_meta_box.php
@@ -11,7 +11,9 @@ if ( ! defined( 'ABSPATH' ) ) {
     add_action( 'woocommerce_product_data_panels', 'wpll_product_panels' );
     add_action( 'add_meta_boxes',  'add_product_custom_image' );
     add_action('woocommerce_process_product_meta', 'wpll_product_custom_fields_save');
-
+    //// Hooks Custom Product variation Filed
+    add_action( 'woocommerce_product_after_variable_attributes', 'wpll_variation_settings_fields', 10, 3 );
+    add_action( 'woocommerce_save_product_variation', 'save_wpll_variation_settings_fields', 10, 2 );
 
     function wpll_custom_product_meta_tab($default_data ){
 
@@ -27,7 +29,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
     /*
      * Product Panel for WPLL Setting
-     *
+     * @since 1.0.0
      * */
     function wpll_product_panels(){
 
@@ -79,20 +81,9 @@ if ( ! defined( 'ABSPATH' ) ) {
             echo "<p><b>" .__( "You need to setup shipping locations in WooCommerce settings ", 'woo-product-country-base-restrictions')." <a href='admin.php?page=wc-settings'> ". __( "HERE", 'woo-product-country-base-restrictions' )."</a> ".__( "before you can choose country restrictions", 'woo-product-country-base-restrictions' )."</b></p>";
         }
 
-//        woocommerce_wp_select([
-//            'id'       => '_type_role_country_list',
-//            'label'    => __( 'Select', 'location-logic' ),
-//            'selected' => true,
-//            'value'    => $selected_country,
-//            'options' => $countries,
-//            'class'     =>  'wplcation_select2',
-//           ' multiple' =>'multiple',
-//        ]);
-
         echo '</div>';
 
     }
-
 
     /*
      * Save All Meta Data Filed
@@ -110,11 +101,107 @@ if ( ! defined( 'ABSPATH' ) ) {
 
     }
 
+    /*
+    * Adding a WPLL settings fields to the WPLL settings for all variation type products
+    *
+    * @since 1.0.0
+    * @para $loop, $variation_data, $variation
+    */
+    function wpll_variation_settings_fields( $loop, $variation_data, $variation ) {
+
+        woocommerce_wp_select(
+            array(
+                'id'      => '_wpll_country_restriction_role_attribute[' . $variation->ID . ']',
+                'label'   => __( 'Rule of Restriction', 'location-logic' ),
+                'default'       => 'all',
+                'class'         => 'availability wpll_restricted_type',
+                'style'			=> 'width:100%;',
+                'value'         => get_post_meta( $variation->ID, '_wpll_country_restriction_role_attribute', true ),
+                'options'       => array(
+                    'all'       => __( 'Available for all countries', 'location-logic' ),
+                    'specific'  => __( 'Available for selected countries', 'location-logic' ),
+                    'excluded'  => __( 'Not Available for selected countries', 'location-logic' ),
+                )
+            )
+        );
+
+
+        $selections = get_post_meta( $variation->ID, '_attribute_restricted_countries', true );
+        if(empty($selections) || ! is_array($selections)) {
+            $selections = array();
+        }
+        $countries_obj   = new WC_Countries();
+        $countries   = $countries_obj->__get('countries');
+        asort( $countries );
+
+        ?>
+        <p class="form-field forminp restricted_countries">
+            <label for="_attribute_restricted_countries[<?php echo $variation->ID; ?>]"><?php echo __( 'Select 
+            countries', 'location-logic' ); ?></label>
+            <select multiple="multiple" name="_attribute_restricted_countries[<?php echo $variation->ID; ?>][]" style="width:100%;max-width: 350px;"
+                    data-placeholder="<?php esc_attr_e( 'Choose countries&hellip;', 'location-logic' ); ?>" title="<?php
+            esc_attr_e( 'Country', 'location-logic' ) ?>"
+                    class="wc-enhanced-select">
+                <?php
+                if ( ! empty( $countries ) ) {
+                    foreach ( $countries as $key => $val ) {
+                        echo '<option value="' . esc_attr( $key ) . '" ' . selected( in_array( $key, $selections ),
+                                true, false ).'>' . $val . '</option>';
+                    }
+                }
+                ?>
+            </select>
+        </p>
+        <?php
+        if( empty( $countries ) ) {
+            echo "<p><b>" .__( "You need to setup shipping locations in WooCommerce settings ", 'location-logic')." <a href='admin.php?page=wc-settings'> ". __( "HERE", 'location-logic' )."</a> ".__( "before you can choose country restrictions", 'location-logic' )."</b></p>";
+        }
+    }
 
     /*
-     * Product Image Change by country
-     *
-     * */
+    * Save the product meta settings for variation product
+    *
+    * @since 1.0.0
+    * @para $post_id
+    */
+    function save_wpll_variation_settings_fields( $post_id ) {
+
+        $restriction = sanitize_text_field($_POST['_wpll_country_restriction_role_attribute'][ $post_id ]);
+
+        if( !isset( $_POST['_attribute_restricted_countries'] ) || empty( $_POST['_attribute_restricted_countries'] ) ) {
+            update_post_meta( $post_id, '_wpll_country_restriction_role_attribute', 'all' );
+        } else {
+            if ( !empty( $restriction ) )
+                update_post_meta( $post_id, '_wpll_country_restriction_role_attribute', $restriction );
+        }
+
+        $countries = array();
+        if(isset($_POST["_attribute_restricted_countries"])) {
+            $countries = wc_clean( $_POST['_attribute_restricted_countries'][ $post_id ] );
+        }
+        update_post_meta( $post_id, '_attribute_restricted_countries', $countries );
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * Product Image Change by country
+ *
+ * */
     function add_product_custom_image() {
         add_meta_box(
             'add_meta_img', __( 'Product Image (Condition)', 'prfx-textdomain' ),
